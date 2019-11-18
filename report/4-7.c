7e2ef05112aeb8fa011eeb3a974b65492b27facd
mirachan010 <mirachan010@gmail.com>
2019-11-18 13:36:46 +0900

    結構ミスってたところ修正

--- report/4-7.c
+++ report/4-7.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 int main(void){
-    int number,i=1;
+    int number,i=1,c=0;
     printf("n‚Ì’lF");
     scanf("%d", &number);
-    do {
-        i=i*2;
-        printf("%d ",i);
-    } while (i<number);
+    if(number>0){
+        printf("1 ");
+        while (number>=i*2) {
+            i=i*2;
+            printf("%d ",i);
+        }
+    }else {printf("1");}
 }
--- report/4-a.c
+++ report/4-a.c
@@ -3,7 +3,7 @@ int main(void){
     int number;
     printf("n‚Ì’lF");
     scanf("%d", &number);
-    for (int i = 0; i < number; i++) {
+    for (int i = 1; i <= number; i++) {
         if (i%3==0) {
             printf("%d ",i);
         }
